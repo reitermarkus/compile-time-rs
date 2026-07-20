19d1767a8a15c616bd4e07d5b774c1cf05a92309
Markus Reiter <me@reitermark.us>
2026-07-20 13:42:14 +0200

    Remove unneeded dependency.

--- macros/src/version.rs
+++ macros/src/version.rs
@@ -1,7 +1,8 @@
-use once_cell::sync::Lazy;
+use std::sync::LazyLock;
+
 use proc_macro2::Span;
 
-static RUSTC_VERSION: Lazy<rustc_version::Result<rustc_version::Version>> = Lazy::new(rustc_version::version);
+static RUSTC_VERSION: LazyLock<rustc_version::Result<rustc_version::Version>> = LazyLock::new(rustc_version::version);
 
 pub fn rustc() -> Result<&'static rustc_version::Version, syn::Error> {
   match &*RUSTC_VERSION {
